274a429cee6dd147d95fc008be35eba3175bbf3c
Manuel J. Sandalio <manueljsandalio@gmail.com>
2022-09-13 03:52:50 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'https://back.winterao.com/api',
-  siteKey: '6LfRObsgAAAAABUcTC5R0ElFhp9C7MDGgJbM2GfK'
+  base_url: '',
+  siteKey: ''
 };
